13aa98eb324e16d245f68d43347486f401da9b7a
Bud Ervin <byron.ervin@leidos.com>
2021-06-15 07:01:23 -0400

    Added output files & CircleCI

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -1,18 +1,20 @@
 #!/usr/bin/env bash
 
-# This tags and uploads an image to Docker Hub
-
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=byronervin/udacity:ml-ops.1.0
+deployment=udacity-ml
+port=8000
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl create deployment $deployment --image=$dockerpath
+kubectl wait --for=condition=available --all deployments
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward deployment/$deployment $port:80
